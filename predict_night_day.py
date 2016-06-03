ca36613293dcfddb3d2e6a92410861efdb4f76ee
= <=>
2016-06-03 16:12:48 -0600

    Simplify night/day training model

--- predict_night_day.py
+++ predict_night_day.py
@@ -8,14 +8,14 @@ from tflearn.data_augmentation import ImageAugmentation
 from sklearn.cross_validation import train_test_split
 import numpy
 from tflearn.layers.core import input_data, dropout, fully_connected
-from tflearn.layers.conv import conv_2d, max_pool_2d
+from tflearn.layers.conv import conv_1d, max_pool_1d
 from tflearn.layers.estimator import regression
 import sys
 from scipy import misc
 
 # Read the image
 filename = sys.argv[1]
-X = [misc.imread(filename)]
+X = [misc.imread(filename, mode='L')]
 X = (numpy.array(X) / 256.0)
 
 ### IS ANY OF THIS NECESSARY FOR LIGHT/DARK? IN GENERAL W/ STAIONARY CAMERA?
@@ -27,29 +27,11 @@ img_aug = ImageAugmentation()
 # img_aug.add_random_flip_leftright()
  
 # Specify shape of the data, image prep
-network = input_data(shape=[None, 26, 32, 3],
+network = input_data(shape=[None, 26, 32],
                      data_preprocessing=img_prep,
                      data_augmentation=img_aug)
 
-# conv_2d incoming, nb_filter, filter_size
-# incoming: Tensor. Incoming 4-D Tensor.
-# nb_filter: int. The number of convolutional filters. # WHAT IS THIS?
-# filter_size: 'intor list ofints`. Size of filters.   # WHAT IS THIS?
-network = conv_2d(network, 64, 3, activation='relu')
-
-# (incoming, kernel_size)
-# incoming: Tensor. Incoming 4-D Layer.
-# kernel_size: 'intor list ofints`. Pooling kernel size.
-network = max_pool_2d(network, 2)
-
-network = conv_2d(network, 64, 3, activation='relu')
-network = conv_2d(network, 64, 3, activation='relu')
-network = max_pool_2d(network, 2)
-
-network = fully_connected(network, 64, activation='relu')
-
-network = dropout(network, 0.5)
-
+network = fully_connected(network, 512, activation='relu')
 network = fully_connected(network, 2, activation='softmax')
 network = regression(network, optimizer='adam',
                      loss='categorical_crossentropy',
--- train_night_day.py
+++ train_night_day.py
@@ -23,7 +23,7 @@ filenames = listdir(image_path)
 
 X = []
 for filename in filenames:
-    image = misc.imread(image_path + '/' + filename)
+    image = misc.imread(image_path + '/' + filename, mode='L')
     X.append(image)
 
 X = (numpy.array(X) / 256.0)
@@ -61,30 +61,13 @@ img_aug = ImageAugmentation()
 # img_aug.add_random_flip_leftright()
  
 # Specify shape of the data, image prep
-network = input_data(shape=[None, 26, 32, 3],
+network = input_data(shape=[None, 26, 32],
                      data_preprocessing=img_prep,
                      data_augmentation=img_aug)
 
-# conv_2d incoming, nb_filter, filter_size
-# incoming: Tensor. Incoming 4-D Tensor.
-# nb_filter: int. The number of convolutional filters. # WHAT IS THIS?
-# filter_size: 'intor list ofints`. Size of filters.   # WHAT IS THIS?
-network = conv_2d(network, 64, 3, activation='relu')
-
-# (incoming, kernel_size)
-# incoming: Tensor. Incoming 4-D Layer.
-# kernel_size: 'intor list ofints`. Pooling kernel size.
-network = max_pool_2d(network, 2)
-
-network = conv_2d(network, 64, 3, activation='relu')
-network = conv_2d(network, 64, 3, activation='relu')
-network = max_pool_2d(network, 2)
-
-network = fully_connected(network, 64, activation='relu')
-
-network = dropout(network, 0.5)
-
+network = fully_connected(network, 512, activation='relu')
 network = fully_connected(network, 2, activation='softmax')
+
 network = regression(network, optimizer='adam',
                      loss='categorical_crossentropy',
                      learning_rate=0.0003)
