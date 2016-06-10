479d38dc678fbd343e3d742112dd9a5228e38a1a
Zach Dicklin <zach@Zachs-MacBook-Air.local>
2016-06-10 09:36:06 +0200

    Use logistic regression with brightness for night/day

--- train_night_day.py
+++ train_night_day.py
@@ -1,27 +1,19 @@
-from network_night_day import build_model_night_day
-import csv
-import numpy
-from sklearn.cross_validation import train_test_split
-from tflearn.data_utils import shuffle
+# http://scikit-learn.org/stable/modules/generated/sklearn.linear_model.LogisticRegression.html
 
-from os import listdir
+import numpy as np
+from sklearn.svm import SVR
+from sklearn.cross_validation import train_test_split
+import matplotlib.pyplot as plt
+from sklearn.metrics import mean_squared_error
+from sklearn import preprocessing
+import csv
 from scipy import misc
+from sklearn import linear_model
 
 image_path = '/Users/zach/Dropbox/machine_learning/image_trainer/training_images_64'
 csv_filename = '/Users/zach/Dropbox/machine_learning/image_trainer/night_day.csv'
 
-
-# Read the images
-filenames = listdir(image_path)
-
-X = []
-for filename in filenames:
-    image = misc.imread(image_path + '/' + filename, mode='L')
-    X.append(image)
-
-X = (numpy.array(X) / 256.0)
-
-# code.interact(local=locals())
+filenames = []
 
 # Read the csv
 f = open(csv_filename, 'rb')
@@ -32,21 +24,37 @@ for idx, row in enumerate(reader):
     # Skip the header row
     if(idx == 0):
         continue
+    filenames.append(row[0])
 
-    rowY = [0,0]
+    y = 0
     if(row[1] == '1'):
-        rowY[0] = 1
-    elif(row[2] == '1'):
-        rowY[1] = 1
-    Y.append(rowY)
+        y = 1
 
-# Split data into training and test sets
-X_train, X_test, y_train, y_test = train_test_split(X, Y, test_size=0.33, random_state=42)
+    Y.append(y)
+Y = np.array(Y)
 
-X_train, y_train = shuffle(X_train, y_train)
+X = np.array([])
+for filename in filenames:
+    image = misc.imread(image_path + '/' + filename, mode='L')
+    avg_brightness = np.matrix(image).mean()
+    X = np.append(X, avg_brightness)
+X = np.array([X]).transpose()
+
+clf = linear_model.LogisticRegression(C=1e5)
+clf.fit(X, Y)
 
 
-model = build_model_night_day()
-model.fit(X_train, y_train, n_epoch=20, shuffle=True, validation_set=(X_test, y_test),
-          show_metric=True, batch_size=100, run_id='night_day_cnn')
-model.save('model_night_day.tflearn')
+# Plot all data
+plt.scatter(X, Y, c='aqua', label='Actual')
+plt.hold('on')
+
+# Plot test predictions
+X_range = range(int(min(X)),int(max(X)))
+X_range = np.array(map(lambda x: [x], X_range))
+plt.plot(X_range, clf.predict(X_range), c='red', label='Prediction', linewidth=1)
+
+plt.xlabel('Brightness')
+plt.ylabel('Nighttime')
+
+plt.legend()
+plt.show()
